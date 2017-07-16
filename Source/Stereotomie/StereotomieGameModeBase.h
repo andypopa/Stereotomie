3a9fa52e12b19691b5faa82c6c453341915bfd9b
Andrei Popa <andypopa@gmail.com>
2017-07-16 18:45:57 +0300

    initial commit

---
+++ Source/Stereotomie/StereotomieGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StereotomieGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEREOTOMIE_API AStereotomieGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
